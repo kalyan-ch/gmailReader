825da782d0fda63220c3550a5ee937324410960c
Kalyan Chavali <kalyan.chavali123@gmail.com>
2019-04-15 21:54:19 -0600

    implemented gmail API to read emails, labels etc.

--- src/main/java/com/scriptNinja/gmailReader/controller/GmailReaderController.java
+++ src/main/java/com/scriptNinja/gmailReader/controller/GmailReaderController.java
@@ -1,16 +1,36 @@
 package com.scriptNinja.gmailReader.controller;
 
+import com.google.api.services.gmail.model.Label;
+import com.scriptNinja.gmailReader.service.GmailReaderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class GmailReaderController {
+
+    @Autowired
+    private GmailReaderService gmailReaderService;
 
     @RequestMapping(value = "/hello")
     public ResponseEntity<String> sayHello(){
         return new ResponseEntity<>("Hey baby", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getEmails")
+    public ResponseEntity<String> getEmails(){
+        StringBuilder sb = new StringBuilder();
+        try{
+            gmailReaderService.searchEmails("");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>(sb.toString(), HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/scriptNinja/gmailReader/service/GmailReaderService.java
@@ -0,0 +1,106 @@
+package com.scriptNinja.gmailReader.service;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Base64;
+import com.google.api.client.util.StringUtils;
+import com.google.api.client.util.Value;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class GmailReaderService {
+
+    @Value("${searchQueries.irea}")
+    private String ireaQuery;
+    @Value("${searchQueries.exel}")
+    private String exelQuery;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    private Gmail service;
+    private static final String APPLICATION_NAME = "gmailReaderApplication";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Collections.singletonList(GmailScopes.MAIL_GOOGLE_COM);
+    private static final String CREDENTIALS_FILE_PATH = "/home/kalyan/ideaProjects/gmailReader/src/main/resources/credentials.json";
+
+
+    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        File file = new File(CREDENTIALS_FILE_PATH);
+        InputStream in = new FileInputStream(file);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    private void authenticateUser() throws Exception{
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public void searchEmails(String query) throws Exception{
+        String searchQuery = "from:foodsby";
+        /*if ("IREA".equalsIgnoreCase(query)) {
+            searchQuery = ireaQuery;
+        }else{
+            searchQuery = exelQuery;
+        }*/
+        authenticateUser();
+        ListMessagesResponse response = service.users().messages().list("me").setQ(searchQuery).execute();
+        for(Message m : response.getMessages()){
+            System.out.println(m.toPrettyString());
+        }
+
+    }
+
+    public List<Label> readLabels() throws Exception{
+
+        authenticateUser();
+        // Print the labels in the user's account.
+        String user = "me";
+        ListLabelsResponse listResponse = service.users().labels().list(user).execute();
+        List<Label> labels = listResponse.getLabels();
+        if (labels.isEmpty()) {
+            System.out.println("No labels found.");
+        } else {
+            System.out.println("Labels:");
+            for (Label label : labels) {
+                System.out.printf("- %s\n", label.getName());
+            }
+        }
+
+        return labels;
+
+    }
+
+
+}
